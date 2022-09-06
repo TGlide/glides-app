30ef60c5b79f57505a1c707942ec905231da9d0b
tglide <26071571+TGlide@users.noreply.github.com>
2022-09-06 22:06:11 +0100

    chore: initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,40 @@
+// src/pages/_app.tsx
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { withTRPC } from "@trpc/next";
+import type { AppType } from "next/dist/shared/lib/utils";
+import { AppRouter } from "server/router";
+
+import superjson from "superjson";
+
+import "styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export default withTRPC<AppRouter>({
+  config() {
+    const url = `${getBaseUrl()}/api/trpc`;
+
+    return {
+      links: [
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === "development" ||
+            (opts.direction === "down" && opts.result instanceof Error),
+        }),
+        httpBatchLink({ url }),
+      ],
+      url,
+      transformer: superjson,
+    };
+  },
+  ssr: false,
+})(MyApp);
