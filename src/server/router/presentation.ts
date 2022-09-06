ff531c9db817946a3fd8df460f6366b2a993375b
tglide <26071571+TGlide@users.noreply.github.com>
2022-09-06 22:41:23 +0100

    feat: basic prisma model logic

---
+++ src/server/router/presentation.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { createRouter } from "./context";
+
+export const presentationRouter = createRouter()
+  .query("hello", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `Hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .query("getAll", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.presentation.findMany();
+    },
+  })
+  .mutation("create", {
+    async resolve({ ctx }) {
+      await ctx.prisma.presentation.create({
+        data: {},
+      });
+    },
+  })
+  .mutation("delete", {
+    input: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      await ctx.prisma.presentation.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    },
+  });
